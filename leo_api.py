771dddde0a493d6566dbad1d1edb7865d81aec95
Daniel Müller Navarro <danielmuellernavarro@gmail.com>
2021-04-15 19:10:03 +0200

    improved library

--- leo_api.py
+++ leo_api.py
@@ -1,102 +1,116 @@
-#!/usr/bin/env python
-#
-# Query the leo.org German/English dictionary.
-#
-# Copyright (C) 2015 Ian Denhardt <ian@zenhack.net>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-import six
-import json
 import requests
 from lxml import etree
 from io import StringIO
-import argparse
-from tabulate import tabulate
+import logging
+import time
 
-arg_parser = argparse.ArgumentParser(
-    description="Query the leo.org German/English dictionary.",
-    epilog="Note that the developers of this software are in "
-           "no way affiliated with leo.org."
-)
-arg_parser.add_argument('query',
-                        metavar='search-term',
-                        nargs='+',
-                        help="word(s) to search for")
-arg_parser.add_argument('-j', '--json',
-                        action='store_true',
-                        default=False,
-                        help='Format results as JSON.')
 
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+uri = 'http://dict.leo.org/%s%s/' # LEO uri
 section_names = (
     'subst',
     'verb',
     'adjadv',
-    'preaep',
+    'praep',
     'definition',
     'phrase',
     'example',
 )
 
 
-def _get_text(elt):
-    buf = StringIO()
+class Leo:
+   def __init__(self, source_language='en', target_language='de', section_names=section_names, wait_between_search=20.0):
+      self.sl = source_language
+      self.tl = target_language
+      self.section_names = section_names
+      self.wait_between_search = wait_between_search
+      self._first_wait = False
+      # English (en)
+      # French (fr)
+      # Spanish (es)
+      # Italian (it)
+      # Chinese (ch)
+      # Russian (ru)
+      # German (de)
+      # Portuguese (pt)
+      # Polish (pl)
 
-    def _helper(_elt):
-        if _elt.text is not None:
-            buf.write(six.text_type(_elt.text))
-        for child in _elt:
-            _helper(child)
-        if _elt.tail is not None:
-            buf.write(six.text_type(_elt.tail))
+   def _get_text(self, elt):
+      buf = StringIO()
 
-    _helper(elt)
-    return buf.getvalue()
+      def _helper(_elt):
+         if _elt.text is not None:
+               buf.write(_elt.text)
+         for child in _elt:
+               _helper(child)
+         if _elt.tail is not None:
+               buf.write(_elt.tail)
 
+      _helper(elt)
+      return buf.getvalue()
 
-def search(term, uri='https://dict.leo.org/ende/'):
-    resp = requests.get(uri, params={'search': term})
-    p = etree.HTMLParser()
-    html = etree.parse(StringIO(resp.text), p)
-    ret = {}
-    for section_name in section_names:
-        section = html.find(".//div[@data-dz-name='%s']" % section_name)
-        if section is None:
+   def _format_ret(self, ret):
+      format_ret = ''
+      for x in ret:
+         # subst, verb, adjadv, definition, example, subst, phrase
+         if x not in self.section_names:
             continue
-        ret[section_name] = []
-        results = section.findall(".//td[@lang='en']")
-        for r_en in results:
-            r_de = r_en.find("./../td[@lang='de']")
+         x[0].upper()
+         format_ret += x + '\n'
+         for x in ret[x]:
+            sl = x[self.sl][0].upper() + x[self.sl][1:]
+            tl = x[self.tl][0].upper() + x[self.tl][1:]
+            format_ret += sl + '\t' + tl + '\n'
+         format_ret += '\n'
+      return format_ret
+
+   def _wait(self):
+      if self._first_wait:
+         time.sleep(self.wait_between_search)
+         self._first_wait = False
+      self._first_wait = True
+
+   def search(self, term, timeout = 10, max_results = 999):
+      self._wait()
+
+      url = uri % (self.sl, self.tl)
+      resp = requests.get(url, params={'search': term}, timeout=timeout)
+      ret = {}
+      if resp.status_code != requests.codes.ok:
+         return ret
+      p = etree.HTMLParser()
+      html = etree.parse(StringIO(resp.text), p)
+      for section_name in self.section_names:
+         section = html.find(".//div[@id='section-%s']" % section_name)
+         if section is None:
+            continue
+         ret[section_name] = []
+         results = section.findall(".//td[@lang='%s']" % (self.sl,)) # source language
+         for index, r_sl in enumerate(results):
+            if index >= max_results:
+               break
+            r_tl = r_sl.find("./../td[@lang='%s']" % (self.tl,)) # target language
             ret[section_name].append({
-                'en': _get_text(r_en).strip(),
-                'de': _get_text(r_de).strip(),
+               self.sl: self._get_text(r_sl).strip(),
+               self.tl: self._get_text(r_tl).strip(),
             })
-    return ret
+      return self._format_ret(ret)
 
 
 def main():
-    args = arg_parser.parse_args()
-    query = ' '.join(args.query)
-    results = search(query)
-    if args.json:
-        print(json.dumps(results))
-    else:
-        for section_name in results.keys():
-            print(section_name)
-            table = [[r['en'], r['de']] for r in results[section_name]]
-            print(tabulate(table, headers=['English', 'German']))
-            print('')  # Extra newline
+   sections = (
+      'subst',
+      'definition',
+      'phrase',
+      'example',
+   )   
+   leo = Leo(section_names=sections)
+   # leo = Leo()
+   result = leo.search('milk', max_results=5)
+   print(result)
+
 
 if __name__ == '__main__':
-    main()
+   main()
